da7f70e8c1a2b4837a29b3d9e6dfbb5dbb12afa8
lenacha <lenacha32@gmail.com>
2019-04-30 10:59:40 -0700

    Bug Fixed

--- datastore/index.js
+++ datastore/index.js
@@ -16,7 +16,7 @@ exports.create = (text, callback) => {
       } else {
         callback(null, { id, text });
       }
-    })
+    });
   });
 };
 
@@ -25,7 +25,7 @@ exports.readAll = (callback) => {
   fs.readdir(exports.dataDir, (err, files) => {
     var data = [];
     if (err) {
-      console.log('NO!')
+      console.log('NO!');
     } else if (files.length === 0) {
       callback(null, data);
     } else {
@@ -36,37 +36,43 @@ exports.readAll = (callback) => {
             console.log('No again');
             callback(err);
           } else {
-            data.push({ id, text })
+            data.push({ id, text });
             if (files.length === data.length) {
               callback(null, data);
             }
           }
-        })
-      })
+        });
+      });
     }
-  })
+  });
 };
 
 exports.readOne = (id, callback) => {
-  fs.readFile(`${exports.dataDir}/${id}.txt`, "utf8", (err, text) => {
+  fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err, text) => {
     if (err) {
       console.log('No files!');
       callback(err);
     } else {
       callback(null, { id, text });
     }
-  })
+  });
 };
 
 exports.update = (id, text, callback) => {
   exports.readOne(id, (err) => {
     if (err) {
-      console.log("Error to update!");
+      console.log('Error to update!');
       callback(new Error(`No item with id: ${id}`));
     } else {
-      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, callback);
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+        if(err) {
+          console.log('Error!');
+        } else {
+          callback(null, {id, text});
+        }
+      });
     }
-  })
+  });
 };
 
 exports.delete = (id, callback) => {
@@ -77,7 +83,7 @@ exports.delete = (id, callback) => {
     } else {
       fs.unlink(`${exports.dataDir}/${id}.txt`, callback);
     }
-  })
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
